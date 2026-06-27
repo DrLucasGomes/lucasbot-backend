e9642dffc5de1674e04e70225187758c3e911511
DrLucasGomes <admin@drlucasgomes.com.br>
2026-06-26 23:38:10 -0300

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ KEY_SUPABASE = os.getenv("SUPABASE_KEY")
 
 # COLA O SEU TOKEN DO MANYCHAT DIRETO DENTRO DAS ASPAS ABAIXO
 # Apague o os.getenv e cole a chave limpa aqui, sem espaços.
-MANYCHAT_TOKEN = "COLE_AQUI_O_SEU_TOKEN_DO_MANYCHAT"
+MANYCHAT_TOKEN = "9730296:e2bbef5228e99eaf17446f519aaa9ca9"
 
 headers_supabase = {
     "apikey": KEY_SUPABASE, 
