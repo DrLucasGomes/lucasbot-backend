1921f3d511d9e979f8778b3e0795b220fb66cbdb
DrLucasGomes <admin@drlucasgomes.com.br>
2026-08-12 23:52:48 -0300

    Register phone capture tracking routes

--- tracking_boot.py
+++ tracking_boot.py
@@ -7,6 +7,8 @@ de rastreamento. O servico de teste no Render deve iniciar este modulo.
 from main import app
 from tracking_routes import router as tracking_router
 from tracking_claim_routes import router as tracking_claim_router
+from tracking_phone_routes import router as tracking_phone_router
 
 app.include_router(tracking_router)
 app.include_router(tracking_claim_router)
+app.include_router(tracking_phone_router)
