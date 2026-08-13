7d8a81de359d3e22bcac1cd349bf1730a77fd0e0
DrLucasGomes <admin@drlucasgomes.com.br>
2026-08-12 23:34:38 -0300

    Register isolated tracking claim routes

--- tracking_boot.py
+++ tracking_boot.py
@@ -6,5 +6,7 @@ de rastreamento. O servico de teste no Render deve iniciar este modulo.
 
 from main import app
 from tracking_routes import router as tracking_router
+from tracking_claim_routes import router as tracking_claim_router
 
 app.include_router(tracking_router)
+app.include_router(tracking_claim_router)
